9489e3e0b794010634fdaaa01c50472dcc338c89
hank.aungkyaw <hank.lo.kyaw@gmail.com>
2024-08-13 14:34:45 -0700

    updated to combine all the Amazon orders into a single PO.

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ import pandas as pd
 import os
 from datetime import datetime as dt
 
+
+def group_amazon_orders(df):
+    # Filter Amazon orders
+    amazon_df = df[df['Vendor'].str.contains('Amazon')]
+
+    # Aggregate Amazon orders into a single line
+    amazon_aggregated = {
+        'Date': amazon_df['Date'].max(),  # Use the latest date
+        'Document Number': amazon_df['Document Number'].min(),  # Use the first document number
+        'Vendor': '1716 Amazon',
+        'Current Status': amazon_df['Current Status'].iloc[0],  # Assuming all status are the same
+        'Total (Transaction)': amazon_df['Total (Transaction)'].sum(),  # Sum all transactions
+        'Memo': 'Weekly Amazon orders'
+    }
+
+    # Create a DataFrame from the aggregated Amazon order
+    amazon_summary_df = pd.DataFrame([amazon_aggregated])
+
+    # Filter out non-Amazon orders
+    non_amazon_df = df[~df['Vendor'].str.contains('Amazon')]
+
+    # Combine non-Amazon orders with the aggregated Amazon order
+    combined_df = pd.concat([non_amazon_df, amazon_summary_df], ignore_index=True)
+
+    return combined_df
+
 today = dt.today().strftime('%m.%d.%Y')
 
 airtable_base_id = os.getenv("PO_BASE_ID")
@@ -13,7 +39,7 @@ weekly_po_requests = fetch_psl_data(airtable_base_id, airtable_table_id, airtabl
 po_df = convert_to_df(weekly_po_requests)
 
 # Filter pending approval requests
-pending_df = po_df[po_df['Status'] == 'Pending Approval'].copy()
+pending_df = po_df[(po_df['Status'] == 'Pending Approval') | (po_df['Status'] == 'Urgent')].copy()
 
 # # Ensure the columns are in numeric format for calculations
 # pending_df.loc[:, 'Unit Cost'] = pd.to_numeric(pending_df['Unit Cost'], errors='coerce')
@@ -43,5 +69,9 @@ else:
     grouped_df['Current Status'] = grouped_df['Current Status'].replace('Pending Approval',
                                                                         'Pending Supervisor Approval')
     grouped_df = grouped_df.sort_values(['Date', 'Document Number'], ascending=[False, True])
+
+    # group amazon orders
+    result_df = group_amazon_orders(grouped_df)
+
     print(grouped_df)
-    grouped_df.to_excel(f"ANA Weekly PO Request {today}.xlsx", index=False)
+    result_df.to_excel(f"ANA Weekly PO Request {today}.xlsx", index=False)
